edf9b3cf2ae7dc16db90355dedbe0ef74c46b08a
SaadDamine <m.sdaminesaad@gmail.com>
2023-08-13 14:13:29 +0100

    fix UserCredentialMapper

--- auth-service/src/main/java/com/damine/authservice/mappers/UserCredentialMapper.java
+++ auth-service/src/main/java/com/damine/authservice/mappers/UserCredentialMapper.java
@@ -1,12 +1,13 @@
 package com.damine.authservice.mappers;
 
 import com.damine.authservice.DTO.UserCredentialDto;
+import com.damine.authservice.DTO.UserCredentialRequestDto;
 import com.damine.authservice.models.UserCredential;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserCredentialMapper {
-    UserCredential from(UserCredentialDto userCredentialDto);
+    UserCredential from(UserCredentialRequestDto userCredentialRequestDto);
 
     UserCredentialDto to(UserCredential userCredential);
 }
