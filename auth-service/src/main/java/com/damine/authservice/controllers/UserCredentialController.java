be35707b69be7d344177b0f2cd70a5f906af4fac
SaadDamine <m.sdaminesaad@gmail.com>
2023-08-13 13:55:31 +0100

    add jwtService -generate and validate token

--- auth-service/src/main/java/com/damine/authservice/controllers/UserCredentialController.java
+++ auth-service/src/main/java/com/damine/authservice/controllers/UserCredentialController.java
@@ -3,14 +3,13 @@ package com.damine.authservice.controllers;
 import com.damine.authservice.DTO.UserCredentialDto;
 import com.damine.authservice.DTO.UserCredentialRequestDto;
 import com.damine.authservice.services.UserCredentialService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +20,16 @@ public class UserCredentialController {
     @PostMapping("register")
     public ResponseEntity<UserCredentialDto> save(@Valid @RequestBody UserCredentialRequestDto userCredentialDto) {
         return new ResponseEntity<>(userCredentialService.saveUser(userCredentialDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping("token")
+    public String getToken(UserCredentialDto userCredentialDto) {
+        return userCredentialService.generateToken(userCredentialDto.getName());
+    }
+
+    @GetMapping("validate")
+    public ResponseEntity<Jws<Claims>> validateToken(@RequestParam("token") String token) {
+        return new ResponseEntity<>(userCredentialService.validateToken(token), HttpStatus.ACCEPTED);
     }
 
 }
--- auth-service/src/main/java/com/damine/authservice/services/UserCredentialService.java
+++ auth-service/src/main/java/com/damine/authservice/services/UserCredentialService.java
@@ -2,9 +2,10 @@ package com.damine.authservice.services;
 
 import com.damine.authservice.DTO.UserCredentialDto;
 import com.damine.authservice.DTO.UserCredentialRequestDto;
-import com.damine.authservice.exceptions.ResourceNotFoundException;
 import com.damine.authservice.mappers.UserCredentialMapper;
 import com.damine.authservice.repositories.UserCredentialRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,13 +17,16 @@ public class UserCredentialService {
     private final UserCredentialRepository userCredentialRepository;
     private final UserCredentialMapper userCredentialMapper;
     private final PasswordEncoder passwordEncoder;
-
+    private final JwtService jwtService;
     public UserCredentialDto saveUser(UserCredentialRequestDto userCredentialRequestDto) {
         userCredentialRequestDto.setPassword(passwordEncoder.encode(userCredentialRequestDto.getPassword()));
         return userCredentialMapper.to(userCredentialRepository.save(userCredentialMapper.from(userCredentialRequestDto)));
     }
+    public String generateToken(String userName){
+        return jwtService.generateToken(userName);
+    }
 
-    public UserCredentialDto findByName(String name) {
-        return userCredentialMapper.to(userCredentialRepository.findByName(name).orElseThrow(() -> new ResourceNotFoundException("Product not found")));
+    public Jws<Claims> validateToken(String token){
+        return jwtService.validateToken(token);
     }
 }
